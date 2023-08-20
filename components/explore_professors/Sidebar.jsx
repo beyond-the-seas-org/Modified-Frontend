08cb3e6b42adb4bb0737de639ddc7ad064aed437
asifhaider <1805112@ugrad.cse.buet.ac.bd>
2023-08-20 21:21:41 +0600

    basic professor ui page added

--- components/explore_professors/Sidebar.jsx
+++ components/explore_professors/Sidebar.jsx
@@ -31,140 +31,140 @@ const Sidebar = ({ mode, setMode }) => {
     >
       <Box position="fixed" width={250} height="100vh" >
         <List>
-        <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-              width: "100%", // Make ListItemButton width 100% of the ListItem
-            }}
->
+          <ListItem disablePadding sx={{
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+            width: "100%", // Make ListItemButton width 100% of the ListItem
+          }}
+          >
             <ListItemButton component="a" href="#home" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
               <ListItemIcon>
                 <Home />
               </ListItemIcon>
               <ListItemText primary={
-                  <Typography variant="body1" fontWeight="bold">
-                    Homepage
-                  </Typography>
-                } />
+                <Typography variant="body1" fontWeight="bold">
+                  Homepage
+                </Typography>
+              } />
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-            }}
->
-              <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
-                <ListItemIcon>
-                  <Article />
-                </ListItemIcon>
-                <ListItemText primary={
-                  <Typography variant="body1" fontWeight="bold">
-                    Prof Shortlist
-                  </Typography>
-                } />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-            }}
->
-              <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
-                <ListItemIcon>
-                  <Group />
-                </ListItemIcon>
-                <ListItemText primary={
-                  <Typography variant="body1" fontWeight="bold">
-                    Groups
-                  </Typography>
-                } />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-            }}
->
-              <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
-                <ListItemIcon>
-                  <PersonSearchIcon />
-                </ListItemIcon>
-                <ListItemText primary={
-                  <Typography variant="body1" fontWeight="bold">
-                    Explore Professors
-                  </Typography>
-                } />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-            }}
->
-              <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
-                <ListItemIcon>
-                  <Settings />
-                </ListItemIcon>
-                <ListItemText primary={
-                  <Typography variant="body1" fontWeight="bold">
-                    Analytics
-                  </Typography>
-                } />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-            }}
->
-              <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
-                <ListItemIcon>
-                  <AccountBox />
-                </ListItemIcon>
-                <ListItemText primary={
-                  <Typography variant="body1" fontWeight="bold">
-                    Profile
-                  </Typography>
-                } />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{
-              "&:hover": {
-                background: "gray",
-                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-              },
-              marginBottom: 4, // Add margin-bottom to create padding between list items
-            }}
->
-              <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
-                <ListItemIcon>
-                  <ModeNight />
-                </ListItemIcon>
-                <Switch onChange={e=>setMode(mode === "light" ? "dark" : "light")}/>
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Box>
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+          }}
+          >
+            <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary={
+                <Typography variant="body1" fontWeight="bold">
+                  Prof Shortlist
+                </Typography>
+              } />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+          }}
+          >
+            <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary={
+                <Typography variant="body1" fontWeight="bold">
+                  Groups
+                </Typography>
+              } />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+          }}
+          >
+            <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
+              <ListItemIcon>
+                <PersonSearchIcon />
+              </ListItemIcon>
+              <ListItemText primary={
+                <Typography variant="body1" fontWeight="bold">
+                  Explore Professors
+                </Typography>
+              } />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+          }}
+          >
+            <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary={
+                <Typography variant="body1" fontWeight="bold">
+                  Analytics
+                </Typography>
+              } />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+          }}
+          >
+            <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary={
+                <Typography variant="body1" fontWeight="bold">
+                  Profile
+                </Typography>
+              } />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{
+            "&:hover": {
+              background: "gray",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            },
+            marginBottom: 4, // Add margin-bottom to create padding between list items
+          }}
+          >
+            <ListItemButton component="a" href="#simple-list" style={{ backgroundColor: bgcolor, color: 'black', border: '4px solid black' }}>
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch onChange={e => setMode(mode === "light" ? "dark" : "light")} />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </Box>
-    );
-  };
-  
-  export default Sidebar;
+    </Box>
+  );
+};
+
+export default Sidebar;
