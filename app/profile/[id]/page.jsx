4cf1abc467da1e1fba7f2734682e4bbac231fc1d
asifhaider <1805112@ugrad.cse.buet.ac.bd>
2023-08-28 02:34:53 +0600

    login redirection done

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,22 +1,57 @@
-'use client'
-import React from 'react'
-import ProfileHeader from '../../../components/profile/ProfileHeader'
-import About from '../../../components/profile/About'
-import Skills from '../../../components/profile/Skills'
-import ActivityFeed from '../../../components/profile/ActivityFeed'
-import Links from '../../../components/profile/Links'
+'use client';
+import React, { useState, useEffect } from 'react';
+import ProfileHeader from '../../../components/profile/ProfileHeader';
+import About from '../../../components/profile/About';
+import Skills from '../../../components/profile/Skills';
+import ActivityFeed from '../../../components/profile/ActivityFeed';
+import Links from '../../../components/profile/Links';
 
-const page = () => {
-  return (
-    <div>
-        <ProfileHeader/>
-        <About/>
-        <Skills/>
-        <Links/>
-       {/* Pass the 'user' prop to the ActivityFeed component */}
-      <ActivityFeed/>
-    </div>
-  )
+const UserProfilePage = () => {
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const qlink = window.location.href;
+    const tokens = qlink.split("/");
+    let user_id = tokens[tokens.length - 1]
+    //convert user id to int
+    console.log("user_id", user_id);
+  
+    user_id = parseInt(user_id);
+    useEffect(() => {
+        async function fetchUserData() {
+            try {
+                // Replace with your API endpoint to fetch user profile information
+                const response = await fetch(`http://127.0.0.1:5002/api/profile/${user_id}`);
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch user data.');
+                }
+
+                const data = await response.json();
+                setUserData(data);
+                setLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        }
+
+        fetchUserData();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    return (
+        <div>
+            <ProfileHeader userData={userData} />
+            <About aboutInfo={userData.about} />
+            <Skills skills={userData.skills} />
+            <Links links={userData.links} />
+            <ActivityFeed user={userData} />
+        </div>
+    );
 }
 
-export default page
+export default UserProfilePage;
