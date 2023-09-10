5a5838d7ddc292910e563e9c9ab8a6ab284a5e28
ANIK115 <aniknj4@gmail.com>
2023-09-10 11:36:27 +0600

    Fixed emptypost issue in newsfeed

--- components/newsfeed/EmptyPost.jsx
+++ components/newsfeed/EmptyPost.jsx
@@ -1,20 +1,8 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { Box, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography, Avatar, Checkbox, Favorite, Button } from "@mui/material";
-import { FavoriteBorder, MoreVert, Share, ChatBubbleOutline, Edit, Delete, ThumbUp, ThumbDown } from "@mui/icons-material";
-//import StyledButton from "../styled-components/StyledButton"
-
+import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 
 const EmptyPost = ({ mode }) => {
-
-
-  // const qlink = window.location.href;
-  // const tokens = qlink.split("/");
-  // let user_id = tokens[tokens.length-1]
-  // //convert user id to int
-  // user_id = parseInt(user_id);
-  // console.log("user_id", user_id);
-
   useEffect(() => {
     const user_id = localStorage.getItem("id");
     if (!user_id) {
@@ -23,33 +11,40 @@ const EmptyPost = ({ mode }) => {
     }
   }, []);
 
+  const [showNoPostsMessage, setShowNoPostsMessage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowNoPostsMessage(true);
+    }, 10000); // 30 seconds
+
+    return () => {
+      clearTimeout(timer); // Clear the timer if the component unmounts
+    };
+  }, []);
+
   return (
+    <div>
     <Card sx={{ 
       margin: 5 , 
-      borderColor:mode === 'dark' ? 'white' : 'black' ,
+      borderColor: mode === 'dark' ? 'white' : 'black' ,
       borderWidth: '2px', 
       borderStyle: 'solid',
       borderRadius: 4,
       boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
-        }
-      } >
+    }}>
       <CardHeader
-        // avatar={ 
-        //   <Avatar alt="Profile Picture" src={professor.image_link} sx={{ width: 60, height: 60 }}/>
-        // }
-
         title={
-          /*Shows the professor's name */
-          <Typography variant="h6" component="div">
-            No Post Found
+          <Typography variant="h6" component="div" align="center">
+            {showNoPostsMessage ? 'No posts found' : 'Loading Posts, Please Wait!'}
           </Typography>
         }
-
-        
       />
-      
-
     </Card>
+    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+    </div>
+    </div>
   );
 };
 
