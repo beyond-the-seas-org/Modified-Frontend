a52e8996b5cf6809ca70a9bb8104995b3c4c12e5
Md. Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2023-09-11 15:07:37 +0600

    tried

--- components/login/Welcome.jsx
+++ components/login/Welcome.jsx
@@ -35,7 +35,7 @@ export default function Welcome() {
                 variant="h5"
                 sx={{
                     position: 'absolute',
-                    top: '18%',  // Adjust this as needed to position the subtitle
+                    top: '30%',  // Adjust this as needed to position the subtitle
                     color: 'white',
                     textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
                 }}
